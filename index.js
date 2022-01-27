15f4f0bc554f8d7eeb76436258d66d48111dcb00
ayoub78170 <ayoub.assiali78@gmail.com>
2022-01-27 16:48:15 +0100

    ajout de modif

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("debut index.js");
+
+function envoieMessage() {
+let message = "mon super message";
+console.log(message);
+// alert(message);
+}
+
+let btn = document.getElementById("monbtn");
+btn.addEventListener("click",envoieMessage);
+// envoieMessage()
+// envoieMessage()
+
+function iMessage() {
+let message = "error 404";
+console.log(message);
+alert(message);
+}
+// reaction a un 
+
+
+
+
+
+
+
+let dbtn = document.getElementById("dbtn");
+btn.addEventListener("click",iMessage);
+
+
+
+console.log("fin index.js");
